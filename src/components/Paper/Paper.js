60b5eb15a503225375c986b6d5f0564552307c74
Juan Jose A <jjalonso@users.noreply.github.com>
2019-05-01 10:33:34 +0100

    Layout welcome v1 screen done, travis cfg removed (#6)

--- src/components/Paper/Paper.js
+++ src/components/Paper/Paper.js
@@ -1,14 +1,15 @@
 import React from 'react';
-import { Typography } from 'antd';
+import classNames from 'classnames';
+// import { Typography } from 'antd';
 
 import styles from './Paper.module.css';
 
-const Paper = ({ children, title }) => (
+const Paper = ({ children, className }) => (
   <>
-    <Typography.Title className={styles.title}>
+    {/* <Typography.Title className={styles.title}>
       { title }
-    </Typography.Title>
-    <div className={styles.paper}>
+    </Typography.Title> */}
+    <div className={classNames(styles.paper, className)}>
       {children}
     </div>
   </>
--- src/components/Welcome/index.js
+++ src/components/Welcome/index.js
@@ -1,52 +1,50 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import {
-  Row,
-  Col,
   Typography,
   Button,
 } from 'antd';
 
 import styles from './Welcome.module.css';
+import Paper from '../Paper/Paper';
 
 const Welcome = () => (
-  <Row>
-    <Col offset={2} span={12} className={styles.text}>
-      <Typography.Title
-        level={2}
-      >
-        Welcome to Carta!
+  <>
+    <Typography.Title
+      level={2}
+      className={styles.pageTitle}
+    >
+      New Assessment
+    </Typography.Title>
+    <Paper className={styles.imagePaper}>
+
+      <Typography.Title level={4}>
+         Welcome to Carta!
       </Typography.Title>
+
       <Typography.Paragraph>
-        As promised, we will help you to generate your assessment in a jiffy.
-        <br />
+        We will help you to generate your assessment in a jiffy.
+      </Typography.Paragraph>
+
+      <Typography.Paragraph>
         We are pretty sure that after trying Carta you will never want
         to type endlessly letters again.
       </Typography.Paragraph>
-      <Typography.Paragraph>
-        We are still BETA and we could still contains errors, please chat with us in case you
-        find some problems or if you fancy to say hi.
-      </Typography.Paragraph>
-      <Typography.Paragraph
-        strong
-      >
+
+      <Typography.Paragraph strong>
         We&apos;ll love to get feedback from you!
       </Typography.Paragraph>
 
       <Link to="/assessment">
         <Button
           type="primary"
+          className={styles.button}
         >
-          Start Assessment!
+          New Assessment
         </Button>
       </Link>
-
-
-    </Col>
-    <Col span={8}>
-      <img className={styles.image} alt="welcome" src="/images/welcome.svg" />
-    </Col>
-  </Row>
+    </Paper>
+  </>
 );
 
 
